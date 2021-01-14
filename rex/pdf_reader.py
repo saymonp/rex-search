0ac6c580939342ed7f5959a4c3ab91f5749199b6
saymonp <saymon.t@outlook.com>
2021-01-13 23:53:01 -0300

    first commit

---
+++ rex/pdf_reader.py
@@ -0,0 +1,29 @@
+from tika import parser
+import re
+
+class PdfReader(Object):
+
+    def __init__(self):
+        pass
+
+    def read(self, file):
+        # Parse data from file
+        file_data = parser.from_file(file)
+        # Get files text content
+        text = file_data["content"]
+        
+        return text
+
+    def read_xml(self, file):
+        # Parse data from file
+        file_data = parser.from_file(file, xmlContent=True)
+        # Get files text content
+        text = file_data["content"]
+
+        return text
+
+    def read_by_page(self, file):
+        parsed_data_full = parser.from_file(file, xmlContent=True)
+        parsed_data_full = parsed_data_full['content']
+
+        return re.findall('<div class="page">(.[\s\S]*?)<\/div>', parsed_data_full)
