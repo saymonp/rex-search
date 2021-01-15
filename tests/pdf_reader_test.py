1ff3174f4ee42179afaffff104f752e212e0af5b
saymonp <saymon.t@outlook.com>
2021-01-14 21:48:49 -0300

    Git ignora txt

--- tests/pdf_reader_test.py
+++ tests/pdf_reader_test.py
@@ -4,19 +4,27 @@ def test_read():
     pdf = PdfReader()
 
     text = pdf.read(r"c:/Users/saymo/Desktop/rex-search/tests/fixtures/test.pdf")
-
     print(text)
+    text_file = open("sample_read.txt", "w", encoding='utf-8')
+    n = text_file.write(text)
+    text_file.close()
 
 def test_read_xml():
     pdf = PdfReader()
 
     text = pdf.read_xml(r"c:/Users/saymo/Desktop/rex-search/tests/fixtures/test.pdf")
 
-    print(text)
+    text_file = open("sample_read_xml.txt", "w", encoding='utf-8')
+    n = text_file.write(text)
+    text_file.close()
 
 def test_read_by_page():
     pdf = PdfReader()
 
     text = pdf.read_by_page(r"c:/Users/saymo/Desktop/rex-search/tests/fixtures/test.pdf")
-
-    print(text)
+    print(len(text))
+    print(text[0])
+    text_file = open("sample_read_by_page.txt", "w", encoding='utf-8')
+    for page in text:
+        n = text_file.write("%s\n" % page)
+    text_file.close()
