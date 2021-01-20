89ca9bdc22a70b2e54df2c52894553722297fc7f
saymonp <saymon.t@outlook.com>
2021-01-20 12:03:17 -0300

    Pesquisa tipo 1

--- rex/data_search.py
+++ rex/data_search.py
@@ -9,7 +9,15 @@ class DataSearch(object):
     def __init__(self):
         pass
 
-    def search(self, data=str, id=int):
+    
+    def search_type_1(self, data=str, id=str):
+        """19967\.97"""
+        employee = f"EMPREGADO - {id} - .+"
+        query = employee+"|\w\w\w\/201[0-8]|ORDENADO \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|REM\. VARIAVEL 1 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|REM\. VARIAVEL 2 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|REM\. VARIAVEL 3 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|ADIANT\.FERIAS \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|FERIAS NORMAIS 1\/3 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))"
+
+        return re.findall(query, data)
+
+    def search_type_2(self, data=str, id=int):
         employee = f"Empregado: {str(id)} .+"
         query = employee+"|ANO: 201[1-8]|MÊS: \d{2}|V01 ORDENADO R\$ (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|VCK REMUNERACAO VARIAVEL 1 R\$ (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|VCM REMUNERACAO VARIAVEL 2 R\$ (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|VCL REMUNERACAO VARIAVEL 3 R\$ (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|VF1 FERIAS NORMAIS R\$ (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|A01 ORDENADO-AC COLETIVO R\$ (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|VF1 SALARIO ADIANTADO FERIAS R\$ (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))"
 
@@ -30,8 +38,20 @@ class DataSearch(object):
             "(.+) R\$ ((?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d)))", data)
 
         return (match.group(1), match.group(2))
+    
+    def get_key_value_type_1(self, data):
+        match = re.search(
+            "(.+) ((?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d)))", data)
 
-    def search_to_csv(self, file=str, id=str):
+        return (match.group(1), match.group(2))
+
+    def search_to_csv(self, file=str, id=str, type=int):
+        if type == 1:
+            return self.search_to_csv_type_1(file, id)
+        if type == 2:
+            return self.search_to_csv_type_2(file, id)
+    
+    def search_to_csv_type_2(self, file=str, id=str):
         pdf = PdfReader()
 
         pages = pdf.read_by_page(file)
@@ -42,7 +62,7 @@ class DataSearch(object):
         first = True
 
         for page in pages:
-            matches = self.search(page, id)
+            matches = self.search_type_2(page, id)
 
             if matches[0].startswith(f"Empregado: {id}"):
                 ordenado = feriasnorm = ordenadocol = remvar1 = remvar1 = remvar2 = remvar3 = salarioadi = 0
@@ -85,7 +105,81 @@ class DataSearch(object):
 
                 tocsv.append(dict_to_csv)
 
-        with open(f'{id}.csv', 'w', newline='') as output_file:
+        with open(f'{id}07.2014-12.2017.csv', 'w', newline='') as output_file:
+            dict_writer = csv.DictWriter(output_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(tocsv)
+
+        return {"ok": id}
+
+    def search_to_csv_type_1(self, file=str, id=str):
+        """EMPREGADO - 19967\.97 - .+|\w\w\w\/201[0-8]|ORDENADO \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|REM\. VARIAVEL 1 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|REM\. VARIAVEL 2 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|REM\. VARIAVEL 3 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|ADIANT\.FERIAS \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))|FERIAS NORMAIS 1\/3 \s* (?:(?<![\d])(?:(?:\d{1,2}\.)*\d{3}|(?:\d{1,3}))\,\d{2}(?!\d))"""
+        pdf = PdfReader()
+
+        pages = pdf.read_by_page(file)
+
+        tocsv = []
+        keys = []
+
+        first = True
+
+        for page in pages:
+            matches = self.search_type_1(page, id)
+            identifier = id.replace("\\", "")
+            
+            if matches[0].startswith(f"EMPREGADO - {identifier}"):
+
+                part_one = [] # "EMPREGADO - "
+                part_two = [] # "JUN/2010"
+
+                for i in range(len(matches)):
+                    if re.match("\w{3}\/\d{4}", matches[i]):
+                        matches_separeted = (part_one[:i], part_two[i:])
+                        break
+
+                
+                for list_matches in matches_separeted:       
+                    ordenado = feriasnorm = ordenadocol = remvar1 = remvar1 = remvar2 = remvar3 = salarioadi = 0
+                    a = f"{self.get_month(matches[2])}/{self.get_year(matches[1])}"
+                    for m in list_matches:
+                        if m.startswith("ORDENADO"):
+                            ordenado += float(self.get_key_value_type_1(m)
+                                            [1].replace(".", "").replace(",", "."))
+                        if m.startswith("VF1 FERIAS NORMAIS"):
+                            feriasnorm += float(self.get_key_value_type_1(m)
+                                                [1].replace(".", "").replace(",", "."))
+                        if m.startswith("A01 ORDENADO-AC COLETIVO"):
+                            ordenadocol += float(self.get_key_value_type_1(m)
+                                                [1].replace(".", "").replace(",", "."))
+                        if m.startswith("REM. VARIAVEL 1"):
+                            remvar1 += float(self.get_key_value_type_1(m)
+                                            [1].replace(".", "").replace(",", "."))
+                        if m.startswith("REM. VARIAVEL 2"):
+                            remvar2 += float(self.get_key_value_type_1(m)
+                                            [1].replace(".", "").replace(",", "."))
+                        if m.startswith("REM. VARIAVEL 3"):
+                            remvar3 += float(self.get_key_value_type_1(m)
+                                            [1].replace(".", "").replace(",", "."))
+                        if m.startswith("FERIAS NORMAIS 1/3"):
+                            salarioadi += float(self.get_key_value_type_1(m)
+                                                [1].replace(".", "").replace(",", "."))
+
+                    dict_to_csv = {matches[0]: f"{self.get_month(matches[2])}/{self.get_year(matches[1])}",
+                                "V01 Ordenado": str(ordenado).replace(".", ",") if ordenado > 0 else "",
+                                "A01 Ordenado AC Coletivo": str(ordenadocol).replace(".", ",") if ordenadocol > 0 else "",
+                                "VF1 Férias Normais": str(feriasnorm).replace(".", ",") if feriasnorm > 0 else "",
+                                "VCK Remuneração Variável 1": str(remvar1).replace(".", ",") if remvar1 > 0 else "",
+                                "VCM Remuneração Variável 2": str(remvar2).replace(".", ",") if remvar2 > 0 else "",
+                                "VCL Remuneração Variável 3": str(remvar3).replace(".", ",") if remvar3 > 0 else "",
+                                "VF1 Salário Adiantado Férias": str(salarioadi).replace(".", ",") if salarioadi > 0 else ""}
+
+                    if first == True:
+                        keys = dict_to_csv.keys()
+                        first = False
+
+                    tocsv.append(dict_to_csv)
+
+        with open(f'{identifier}-01.2010-06.2014.csv', 'w', newline='') as output_file:
             dict_writer = csv.DictWriter(output_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(tocsv)
--- tests/data_search_test.py
+++ tests/data_search_test.py
@@ -1,17 +1,24 @@
 from rex.data_search import DataSearch
 
 def test_search():
-    with open('c:/Users/saymo/Desktop/rex-search/test.txt', "r", encoding='utf-8') as f:
+    with open('c:/Users/saymo/Desktop/rex-search/FOLHA DE PAGAMENTO TIPO1_read_by_page.txt', "r", encoding='utf-8') as f:
         text = f.read()
     
     dt = DataSearch()
 
-    response = dt.search(text, 37709)
+    response = dt.search_type_1(text, "37709\.31")
 
     print(response)
 
-def test_search_to_csv():
+def test_search_to_csv_type_2():
     file = 'c:/Users/saymo/Desktop/rex-search/tests/fixtures/FOLHA DE PAGAMENTO - tipo2.pdf'
     
     dt = DataSearch()
-    dt.search_to_csv(file, 37709)
+    dt.search_to_csv(file, "37709", type=2)
+
+def test_search_to_csv_type_1():
+    file = 'c:/Users/saymo/Desktop/rex-search/tests/fixtures/FOLHA DE PAGAMENTO - tipo1.pdf'
+    
+    dt = DataSearch()
+    dt.search_to_csv(file, "37709\.31", type=1)
+
